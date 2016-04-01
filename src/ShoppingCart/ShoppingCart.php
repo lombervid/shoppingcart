a43a87f46a3e0235576acd4f280ad749367130bc
David Lombera <david@dropstudio.mx>
2016-04-01 17:38:10 -0600

    Change shopping cart to 2.0

--- src/ShoppingCart/ShoppingCart.php
+++ src/ShoppingCart/ShoppingCart.php
@@ -1,27 +1,24 @@
 <?php
 /**
- * This is a PHP library for creating a simple shopping cart.
+ * This is a PHP library to create a simple shopping cart.
  *
- * @copyright Copyright (c) 2015, Codelutions.
+ * @copyright Copyright (c) 2016, Codelutions.
  * @link      https://github.com/lombervid/shoppingcart
- * 
+ *
  * @package   ShoppingCart
  */
 namespace ShoppingCart;
 
-if (!is_session_started()) {
-    session_start();
-}
 /**
  * ShoppingCart Class
- * @version 1.0
+ * @version 2.0
  */
 class ShoppingCart
 {
     /**
      * @const string Version of this library.
      */
-    const VERSION = '1.0.0';
+    const VERSION = '2.0';
 
     /**
      * @var array Array of items.
@@ -29,33 +26,42 @@ class ShoppingCart
     protected $items;
 
     /**
-     * @var string Name of the Session.
+     * @var array Cart options.
      */
-    protected $name;
+    protected $options = array(
+        'name'     => 'shopping_cart',
+        'autosave' => true,
+        'shipping' => array(
+            'amount' => 0,
+            'free'   => 0,
+        ),
+        'tax'      => 0,
+        'optionas' => 'value',
+    );
 
     /**
      * Constructor.
-     * 
+     *
      * @param string $name Name of the Session.
      */
-    public function __construct($name = 'shopping_cart')
+    public function __construct(array $options = array())
     {
-        if (!is_string($name)) {
-            throw new Exception('Param $name must be string.', 1);            
-        }
+        $this->items   = array();
+        $this->options = $this->filterOptions($options);
 
-        $this->name = $name;
 
-        if (!empty($_SESSION[$this->name]) && is_array($_SESSION[$this->name])) {
-            $this->items = $_SESSION[$this->name];
-        } else {
-            $this->items = array();            
-        } 
+        // $this->name = $name;
+
+        // if (!empty($_SESSION[$this->name]) && is_array($_SESSION[$this->name])) {
+        //     $this->items = $_SESSION[$this->name];
+        // } else {
+        //     $this->items = array();
+        // }
     }
 
     /**
      * Return the items
-     * 
+     *
      * @return array Items in the shopping cart
      */
     public function items()
@@ -69,12 +75,13 @@ class ShoppingCart
     public function clean()
     {
         $this->items = array();
-        $this->save();
+
+        return $this;
     }
 
     /**
      * Checks if the cart is empty
-     * 
+     *
      * @return  boolean Return true if there are not items in the cart, otherwise false
      */
     public function isEmpty()
@@ -84,7 +91,7 @@ class ShoppingCart
 
     /**
      * Checks the total items in the cart
-     * 
+     *
      * @return integer Total items in the cart
      */
     public function numItems()
@@ -94,71 +101,95 @@ class ShoppingCart
 
     /**
      * Add an item in the cart
-     * 
+     *
      * @param integer   $id         Item ID to add
      * @param integer   $amount     Amount to add
      * @param array     $fields     Extra fields
      */
-    public function add($id, $amount = 1, $fields = array(), $exc = array())
-    {
-        if (!is_integer($id) && !is_string($id)) {
-            throw new Exception('Params $id must be integer or string.', 1);            
-        }
+    // public function add($id, $amount = 1, $fields = array(), $exc = array())
+    // {
+    //     if (!is_integer($id) && !is_string($id)) {
+    //         throw new Exception('Params $id must be integer or string.', 1);
+    //     }
 
-        if (!is_numeric($amount)) {
-            throw new Exception('Params $amount must be integer.', 1); 
-        }
+    //     if (!is_numeric($amount)) {
+    //         throw new Exception('Params $amount must be integer.', 1);
+    //     }
 
-        if (!is_array($fields)) {
-            throw new Exception('Params $fields must be array.', 1); 
-        }
+    //     if (!is_array($fields)) {
+    //         throw new Exception('Params $fields must be array.', 1);
+    //     }
 
-        if (!is_array($exc)) {
-            throw new Exception('Params $exc must be array.', 1); 
-        }
+    //     if (!is_array($exc)) {
+    //         throw new Exception('Params $exc must be array.', 1);
+    //     }
 
-        $u_id = md5($id);
+    //     $u_id = md5($id);
 
-        if (array_key_exists($u_id, $this->items)) {
-            $this->items[$u_id]['amount'] += intval($amount);
-        } else {
-            $this->items[$u_id] = array(
-                'id'        =>  $id,
-                'amount'    =>  intval($amount)
-            );
-            foreach ($fields as $field => $value) {
-                if (!in_array($field, $exc)) {
-                    $this->items[$u_id][$field] = $value;
-                }
-            }
-        }
-        $this->save();
-    }
+    //     if (array_key_exists($u_id, $this->items)) {
+    //         $this->items[$u_id]['amount'] += intval($amount);
+    //     } else {
+    //         $this->items[$u_id] = array(
+    //             'id'        =>  $id,
+    //             'amount'    =>  intval($amount)
+    //         );
+    //         foreach ($fields as $field => $value) {
+    //             if (!in_array($field, $exc)) {
+    //                 $this->items[$u_id][$field] = $value;
+    //             }
+    //         }
+    //     }
+    //     $this->save();
+    // }
 
     /**
      * Delete a item from the cart
-     * 
+     *
      * @param  integer $id Item ID to delete
      */
-    public function delete($id)
-    {
-        if (!is_integer($id) && !is_string($id)) {
-            throw new Exception('Params $id must be integer or string.', 1);            
-        }
+    // public function delete($id)
+    // {
+    //     if (!is_integer($id) && !is_string($id)) {
+    //         throw new Exception('Params $id must be integer or string.', 1);
+    //     }
 
-        $u_id = md5($id);
+    //     $u_id = md5($id);
 
-        if (array_key_exists($u_id, $this->items)) {
-            unset($this->items[$u_id]);
-            $this->save();
-        }
-    }
+    //     if (array_key_exists($u_id, $this->items)) {
+    //         unset($this->items[$u_id]);
+    //         $this->save();
+    //     }
+    // }
 
     /**
      * Save the items in the Session.
      */
-    protected function save()
+    public function save()
     {
         $_SESSION[$this->name] = $this->items;
+    }
+
+    protected function filterOptions(array $options)
+    {
+        return array_replace_recursive(
+            $this->options,
+            array_intersect_key_recursive($options, $this->options)
+        );
+    }
+
+    protected function getOption($name)
+    {
+        if (!array_key_exists($name, $this->options)) {
+            throw new Exception('Invalid option.', 1);
+        }
+
+        return $this->options[$name];
+    }
+
+    protected function __destruct()
+    {
+        if ($this->getOption('autosave')) {
+            $this->save();
+        }
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -1,15 +1,21 @@
 <?php
 /**
- * Check if the session has already been started
- * @return boolean
- */
-function is_session_started() {
-    if ( php_sapi_name() !== 'cli' ) {
-        if ( version_compare(PHP_VERSION, '5.4.0', '>=') ) {
-            return (session_status() === PHP_SESSION_ACTIVE);
-        } else {
-            return (session_id() !== '');
+* Recursively computes the intersection of arrays using keys for comparison.
+*
+* @param   array $array1 The array with master keys to check.
+* @param   array $array2 An array to compare keys against.
+* @return  array associative array containing all the entries of array1 which have keys that are present in array2.
+**/
+function array_intersect_key_recursive(array $array1, array $array2) {
+    $array1 = array_intersect_key($array1, $array2);
+    foreach ($array1 as $key => &$value) {
+        if (is_array($array2[$key])) {
+            if (is_array($value)) {
+                $value = array_intersect_key_recursive($value, $array2[$key]);
+            } else {
+                unset($array1[$key]);
+            }
         }
     }
-    return false;
+    return $array1;
 }
