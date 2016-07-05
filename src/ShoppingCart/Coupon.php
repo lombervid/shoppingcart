e54b966a67c27af599bcdf64b0173d2f575033d3
David Lombera <david@dropstudio.mx>
2016-07-05 13:46:14 -0500

    Fix coupon properties names

--- src/ShoppingCart/Coupon.php
+++ src/ShoppingCart/Coupon.php
@@ -5,19 +5,22 @@ class Coupon
 {
 	private $code;
 	private $price;
-	private $order;
+	private $percentage;
+	private $inCart;
 	private $items;
 
 	public function __construct(
 		$code,
 		$price,
+		$percentage = 0,
 		$inCart = 1,
 		array $items = array()
 	) {
-		$this->code   = $code;
-		$this->price  = $price;
-		$this->inCart = $inCart;
-		$this->items  = $items;
+		$this->code       = $code;
+		$this->price      = $price;
+		$this->percentage = $percentage;
+		$this->inCart     = $inCart;
+		$this->items      = $items;
 	}
 
 	public function set(
