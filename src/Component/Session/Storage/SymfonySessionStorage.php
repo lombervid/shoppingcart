fb5e0b0e490b6fe4f746caf7c71c2a265196ded8
David Lombera <david.lomberlan@gmail.com>
2023-05-19 17:15:46 -0600

    refactor!: drop support for `SymfonySessionStorage`

--- src/Component/Session/Storage/SymfonySessionStorage.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Lombervid\ShoppingCart\Component\Session\Storage;
-
-use Symfony\Component\HttpFoundation\Session\Session as SymfonySession;
-
-class SymfonySessionStorage extends SymfonySession implements SessionStorageInterface
-{
-
-}
