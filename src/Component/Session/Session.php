780d3fc40601515210cec45af92bd8f162334d03
David Lombera <david.lomberlan@gmail.com>
2023-05-19 14:29:27 -0600

    style: convert tabs to spaces

--- src/Component/Session/Session.php
+++ src/Component/Session/Session.php
@@ -6,31 +6,31 @@ use Lombervid\ShoppingCart\Component\Session\Storage\NativeSessionStorage;
 
 class Session
 {
-	protected $storage;
+    protected $storage;
 
-	public function __construct(SessionStorageInterface $storage = null)
-	{
-		$this->storage = $storage ?: new NativeSessionStorage();
-		$this->storage->start();
-	}
+    public function __construct(SessionStorageInterface $storage = null)
+    {
+        $this->storage = $storage ?: new NativeSessionStorage();
+        $this->storage->start();
+    }
 
-	public function set($name, $value)
-	{
-		$this->storage->set($name, $value);
-	}
+    public function set($name, $value)
+    {
+        $this->storage->set($name, $value);
+    }
 
-	public function get($name)
-	{
-		return $this->storage->get($name);
-	}
+    public function get($name)
+    {
+        return $this->storage->get($name);
+    }
 
-	public function remove($name)
-	{
-		$this->storage->remove($name);
-	}
+    public function remove($name)
+    {
+        $this->storage->remove($name);
+    }
 
-	public function clear()
-	{
-		$this->storage->clear();
-	}
+    public function clear()
+    {
+        $this->storage->clear();
+    }
 }
--- src/Item.php
+++ src/Item.php
@@ -3,79 +3,79 @@ namespace Lombervid\ShoppingCart;
 
 class Item
 {
-	private $id;
-	private $name;
-	private $price;
-	private $qty;
-	private $fields;
-	private $discount;
+    private $id;
+    private $name;
+    private $price;
+    private $qty;
+    private $fields;
+    private $discount;
 
-	public function __construct(
-		$id,
-		$name,
-		$price,
-		$qty = 1,
-		array $fields = array(),
-		$discount = 0
-	) {
-		$this->id       = strval($id);
-		$this->name     = strval($name);
-		$this->price    = floatval($price);
-		$this->qty      = intval($qty);
-		$this->fields   = $fields;
-		$this->discount = floatval($discount);
-	}
+    public function __construct(
+        $id,
+        $name,
+        $price,
+        $qty = 1,
+        array $fields = array(),
+        $discount = 0
+    ) {
+        $this->id       = strval($id);
+        $this->name     = strval($name);
+        $this->price    = floatval($price);
+        $this->qty      = intval($qty);
+        $this->fields   = $fields;
+        $this->discount = floatval($discount);
+    }
 
-	public function add($qty)
-	{
-		$this->qty += intval($qty);
-	}
+    public function add($qty)
+    {
+        $this->qty += intval($qty);
+    }
 
-	public function update($qty)
-	{
-		$this->qty = intval($qty);
-	}
+    public function update($qty)
+    {
+        $this->qty = intval($qty);
+    }
 
-	public function get($name)
-	{
-		if ('fields' != $name && property_exists($this, $name)) {
-			return $this->{$name};
-		} elseif (array_key_exists($name, $this->fields)) {
-			return $this->fields[$name];
-		}
+    public function get($name)
+    {
+        if ('fields' != $name && property_exists($this, $name)) {
+            return $this->{$name};
+        } elseif (array_key_exists($name, $this->fields)) {
+            return $this->fields[$name];
+        }
 
-		return '';
-	}
+        return '';
+    }
 
-	public function hasDiscount()
-	{
-		return (0 < $this->discount);
-	}
+    public function hasDiscount()
+    {
+        return (0 < $this->discount);
+    }
 
-	public function price()
-	{
-		return $this->price - $this->discount();
-	}
+    public function price()
+    {
+        return $this->price - $this->discount();
+    }
 
-	public function total()
-	{
-		return $this->price() * $this->qty;
-	}
+    public function total()
+    {
+        return $this->price() * $this->qty;
+    }
 
     public function toArray()
     {
         return array(
-			'id'       => $this->id,
-			'name'     => $this->name,
-			'price'    => $this->price,
-			'qty'      => $this->qty,
-			'discount' => $this->discount,
-			'fields'   => $this->fields,
+            'id'       => $this->id,
+            'name'     => $this->name,
+            'price'    => $this->price,
+            'qty'      => $this->qty,
+            'discount' => $this->discount,
+            'fields'   => $this->fields,
         );
     }
 
-	private function discount()
-	{
-		return $this->discount;
-	}
+    private function discount()
+    {
+        return $this->discount;
+    }
 }
