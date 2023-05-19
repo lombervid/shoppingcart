4b3f9d5c340c7763d0c5e91df5a639161f945ff3
David Lombera <david.lomberlan@gmail.com>
2023-05-19 14:29:27 -0600

    style: convert tabs to spaces

--- src/Component/Session/Storage/SessionStorageInterface.php
+++ src/Component/Session/Storage/SessionStorageInterface.php
@@ -3,10 +3,10 @@ namespace Lombervid\ShoppingCart\Component\Session\Storage;
 
 interface SessionStorageInterface
 {
-	public function start();
-	public function isStarted();
-	public function set($name, $value);
-	public function get($name);
-	public function remove($name);
-	public function clear();
+    public function start();
+    public function isStarted();
+    public function set($name, $value);
+    public function get($name);
+    public function remove($name);
+    public function clear();
 }
