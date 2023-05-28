1db41700614f1ea4012819325c591a3525e7fc11
David Lombera <david.lomberlan@gmail.com>
2023-05-28 14:48:57 -0600

    feat: add `$default` parameter to `StorageInterface::get()` method

--- src/Component/Storage/StorageInterface.php
+++ src/Component/Storage/StorageInterface.php
@@ -5,7 +5,7 @@ namespace Lombervid\ShoppingCart\Component\Storage;
 interface StorageInterface
 {
     public function set(string $name, mixed $value): void;
-    public function get(string $name): string;
+    public function get(string $name, mixed $default = null): mixed;
     public function remove(string $name): void;
     public function clear(): void;
 }
